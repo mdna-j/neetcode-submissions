ad4013454c6196e94c90df68907249a1d53b0304
Jose Medina <mdnajjose@gmail.com>
2026-07-08 11:53:49 -0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i  # Found the target, return its index
+        return -1  # Target does not exist in the array
